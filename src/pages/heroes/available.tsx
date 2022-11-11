c9941806d460b5bb31c48a067b3b032699650e7b
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2022-11-10 20:45:20 -0600

    add all & available tabs to heroes pages & pretty up

--- src/pages/heroes/available.tsx
+++ src/pages/heroes/available.tsx
@@ -1,22 +1,36 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import HeroPreview from "../../components/Heroes/HeroPreview";
 import { trpc } from "../../utils/trpc";
 
 const HeroesPage: NextPage = () => {
     const heroes = trpc.hero.getHeroesByGuild.useQuery({ id: "0" });
+    const guildId = sessionStorage.getItem("guild");
+
     return (
         <div>
-            <h1>Available Heroes</h1>
-            <div className="heroes-list grid grid-cols-5 gap-4 p-4">
-                {!heroes?.data && <p>Loading...</p>}
+            <h1 className="text-2xl my-8">Available Heroes</h1>
+            <div className="tabs-container">
+                <ul className="tabs">
+                    <li className="tab"><Link href="/heroes">All</Link></li>
+                    <li className="tab active"><Link href="#">Available</Link></li>
+                </ul>
+            </div>
+            {!heroes?.data && <p>Loading...</p>}
+            <div className="cards">
                 {heroes?.data && heroes.data.map((hero, i) => {
                     return (
-                        <div className="hero" key={i}>
+                        <div className="card" key={i}>
                             <HeroPreview {...hero} />
                         </div>
                     )
                 })}
             </div>
+            {guildId && (
+                <div className="mt-8">
+                    <Link href={`/guild/${guildId}`} className="btn">Back to guild</Link>
+                </div>
+            )}
         </div>
     )
 }
--- src/pages/heroes/index.tsx
+++ src/pages/heroes/index.tsx
@@ -1,22 +1,44 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import HeroPreview from "../../components/Heroes/HeroPreview";
 import { trpc } from "../../utils/trpc";
 
 const HeroesPage: NextPage = () => {
-    const heroes = trpc.hero.getAll.useQuery()?.data;
+    const { data, error, isError, isLoading } = trpc.hero.getAll.useQuery();
+    const guildId = sessionStorage.getItem("guild");
+    
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (isError) {
+        console.log('error: ', error);
+        return <div>Error</div>
+    }
 
     return (
         <div>
-            <h1>All Heroes</h1>
-            <div className="heroes-list flex p-4">
-                {heroes && heroes.map((hero, i) => {
+            <h1 className="text-2xl my-8">All Heroes</h1>
+            <div className="tabs-container">
+                <ul className="tabs">
+                    <li className="tab active"><Link href="#">All</Link></li>
+                    <li className="tab"><Link href="/heroes/available">Available</Link></li>
+                </ul>
+            </div>
+            <div className="cards">
+                {data && data.map((hero, i) => {
                     return (
-                        <div className="hero p-4" key={i}>
+                        <div className="card" key={i}>
                             <HeroPreview {...hero} />
                         </div>
                     )
                 })}
             </div>
+            {guildId && (
+                <div className="mt-8">
+                    <Link href={`/guild/${guildId}`} className="btn">Back to guild</Link>
+                </div>
+            )}
         </div>
     )
 }
