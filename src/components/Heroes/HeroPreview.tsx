3c29c941d67009c43a31115a2732d948a8292a39
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2022-11-10 20:43:07 -0600

    pretty up hero preview

--- src/components/Heroes/HeroPreview.tsx
+++ src/components/Heroes/HeroPreview.tsx
@@ -3,31 +3,41 @@ import Link from "next/link";
 
 const HeroPreview = (props: Hero): JSX.Element => {
     return (
-        <div className="hero-preview">
-            <h2>Name: {props.name}</h2>
-            <div>Race: {props.race}</div>
-            <div>Class: {props.class}</div>
-            <div>Guild: {props.guildId}</div>
-            <table>
+        <Link  href={`/heroes/${props.id}`}>
+            <div className="flex justify-between p-4">
+                <div className="mr-4 bg-yellow-100">
+                    <div>Level: {props.level}</div>
+                    <div>Race: {props.race}</div>
+                    <div>Class: {props.class}</div>
+                </div>
+                <img src="https://via.placeholder.com/100?text=Hero" alt="hero preview thumbnail" />
+            </div>
+            <div className="bg-gray-300">
+            <table className="text-center mx-auto w-full border border-red-900 border-separate">
+                <caption className="text-lg my-2">{props.name}</caption>
                 <thead>
                     <tr>
-                        <th>Str</th>
-                        <th>Dex</th>
-                        <th>Mag</th>
-                        <th>Con</th>
+                        <th className="table-cell">Str</th>
+                        <th className="table-cell">Dex</th>
+                        <th className="table-cell">Mag</th>
+                        <th className="table-cell">Con</th>
+                        <th className="table-cell">Def</th>
+                        <th className="table-cell">Res</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>{props.strength}</td>
-                        <td>{props.dexterity}</td>
-                        <td>{props.magic}</td>
-                        <td>{props.constitution}</td>
+                        <td className="table-cell">{props.strength}</td>
+                        <td className="table-cell">{props.dexterity}</td>
+                        <td className="table-cell">{props.magic}</td>
+                        <td className="table-cell">{props.constitution}</td>
+                        <td className="table-cell">{props.defense}</td>
+                        <td className="table-cell">{props.resistance}</td>
                     </tr>
                 </tbody>
             </table>
-            <Link href={`/heroes/${props.id}`}>Details</Link>
-        </div>
+            </div>
+        </Link>
     )
 }
 
