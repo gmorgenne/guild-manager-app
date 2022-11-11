59074d512e8e6f3eab03f9994fc140b215a01572
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2022-11-10 20:44:36 -0600

    pretty up home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,8 +71,10 @@ const AuthShowcase: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-2">
       {sessionData && (
-        <p className="text-2xl text-blue-500">
+        <p className="text-2xl text-blue-500 text-center">
           Logged in as {sessionData?.user?.name}
+          <br />
+          <Link href="/guild" className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100">Go To Guild</Link>
         </p>
       )}
       {secretMessage && (
