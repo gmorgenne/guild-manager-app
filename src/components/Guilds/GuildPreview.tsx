bb34e0bb11497a30b504beb97b70959f7b9abacd
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2022-11-10 20:42:47 -0600

    pretty up guild preview

--- src/components/Guilds/GuildPreview.tsx
+++ src/components/Guilds/GuildPreview.tsx
@@ -2,11 +2,17 @@ import type { Guild } from "@prisma/client";
 
 const GuildPreview = (guild: Guild): JSX.Element => {
     return (
-        <div className="guild-preview">
-            <div className="guild-name">Guild Name: {guild.name}</div>
-            <div className="guild-id">Guild id: {guild.id}</div>
-            <div className="guild-primary-color">Guild primary color: {guild.primaryColor} <div style={{backgroundColor: guild.primaryColor, width: 15}}>&nbsp;</div></div>
-            <div className="guild-secondary-color">Guild secondary color: {guild.secondaryColor} <div style={{backgroundColor: guild.secondaryColor, width: 15}}>&nbsp;</div></div>
+        <div className="guild-preview p-4">
+            <div className="guild-name text-left">Name: {guild.name}</div>
+            <div className="guild-id">ID: {guild.id}</div>
+            <div className="guild-primary-color flex justify-between">
+                <span>Primary: {guild.primaryColor}</span>
+                <div className="ml-2 border border-black" style={{backgroundColor: guild.primaryColor, width: 25}}>&nbsp;</div>
+            </div>
+            <div className="guild-secondary-color flex justify-between">
+                <span>Secondary: {guild.secondaryColor} </span>
+                <div className="ml-2 border border-black" style={{backgroundColor: guild.secondaryColor, width: 25}}>&nbsp;</div>
+            </div>
         </div>
     )
 }
