1835543a04be50e44fdaa3cd674eb1daec62210f
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2022-11-13 20:22:44 -0600

    adding display of new guild data and active party component

---
+++ src/components/Guilds/ActiveParties.tsx
@@ -0,0 +1,12 @@
+const ActiveParties = (): JSX.Element => {
+    // get party ids by guild
+    // get party by id
+    // get heroes by party id
+    // get quest by party.questId
+    
+    return (
+        <h2 className="text-xl my-8">Active Party List</h2>
+    )
+}
+
+export default ActiveParties;
--- src/pages/guild/[id].tsx
+++ src/pages/guild/[id].tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import ActiveParties from "../../components/Guilds/ActiveParties";
 import GuildPreview from "../../components/Guilds/GuildPreview";
 import PartyBuilder from "../../components/Guilds/PartyBuilder";
 import { trpc } from "../../utils/trpc";
@@ -9,21 +10,50 @@ const GuildPage: NextPage = () => {
     const { asPath } = useRouter();
     const id = asPath.split('/').pop();
     const guild = trpc.guild.getGuildById.useQuery({ id: id })?.data;
-    // TODO: display active parties with quest info
-    // TODO: determine other info for guild screen: facilities, guild upgrades, world/city view, contract expiration warnings, party actions? (training, material gathering, studying, drinking to boost bond/morale, regular quest, league quest)
+    // TODO: display active parties with activity info (quest, training, etc...)
+    // TODO: determine other info for guild screen: facilities, guild upgrades, world/city view, contract expiration warnings, 
+    // TODO: other party actions? (material gathering, studying, drinking to boost bond/morale, regular quest, league quest)
 
     return (
         <div>
             {guild && (
                 <div>
                     <h1 className="text-2xl my-8">Guild Overview</h1>
-                    <div className="p-4 rounded border-2 border-gray-500 max-w-prose">
-                        <GuildPreview {...guild} />
+                    <div className="md:flex">
+                        <div className="p-4 rounded border-2 border-gray-500 max-w-1/2">
+                            <GuildPreview {...guild} />
+                            <span>Created Date: {guild.createdDate.toLocaleDateString("en-us")}</span>
+                        </div>
+                        <table className="text-center md:ml-8">
+                            <caption className="text-lg my-2">Guild Facilities</caption>
+                            <thead>
+                                <tr>
+                                    <th className="table-cell">Mess Hall</th>
+                                    <th className="table-cell">Workshop</th>
+                                    <th className="table-cell">Training Ground</th>
+                                    <th className="table-cell">Barracks</th>
+                                    <th className="table-cell">Infirmary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="table-cell">{guild.messHallLevel}</td>
+                                    <td className="table-cell">{guild.workshopLevel}</td>
+                                    <td className="table-cell">{guild.trainingGroundLevel}</td>
+                                    <td className="table-cell">{guild.barracksLevel}</td>
+                                    <td className="table-cell">{guild.infirmaryLevel}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className="md:ml-8 bg-yellow-400 text-center p-4">
+                            <span>Guild Purse: {guild.purse}</span>
+                        </div>
                     </div>
                     <div className="flex">
                         <Link href="/heroes/available" className="btn">Add Hero</Link>
                         <Link href={`/guild/heroes/${guild.id}`} className="btn">Guild Hero Roster</Link>
                     </div>
+                    <ActiveParties />
                     <PartyBuilder />
                 </div>
             )}
