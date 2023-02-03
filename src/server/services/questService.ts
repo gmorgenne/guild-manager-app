719467aa59d32ec87da8dfdfc53ced14cb8fd5d6
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2023-02-03 17:02:44 -0600

    add notes and format quest service

--- src/server/services/questService.ts
+++ src/server/services/questService.ts
@@ -10,7 +10,7 @@ export const createQuest = async (input: Prisma.QuestCreateInput) => {
     }));
 }
 
-export const generateQuest = async () : Promise<Prisma.QuestCreateInput> => {
+export const generateQuest = async (): Promise<Prisma.QuestCreateInput> => {
     // TODO: add optional input that can be used to create quests for certain municipalities
     const sex = getRandomBool();
     const race = randomFromArray(Races, "Human");
@@ -18,29 +18,30 @@ export const generateQuest = async () : Promise<Prisma.QuestCreateInput> => {
     let purseGain = 0;
 
     const encounterIds: string[] = [];
-    const encounterConnect: Prisma.Enumerable<Prisma.EncounterWhereUniqueInput> = [];  
+    const encounterConnect: Prisma.Enumerable<Prisma.EncounterWhereUniqueInput> = [];
     const encountersToCreate = getRandomInt(1, 4);
+    // compute a severity level to use for item drop?
     for (let i = 1; i < encountersToCreate; i++) {
         const encounter = await createEncounterHandler().then((response) => { return response?.data?.encounter });
         if (encounter?.id)
             encounterIds.push(encounter?.id);
         if (encounter?.purseGain)
             purseGain += encounter?.purseGain;
-    } 
-    
-   encounterIds.forEach((encounterId) => {
+    }
+
+    encounterIds.forEach((encounterId) => {
         encounterConnect.push({
             id: encounterId
         });
-   });
+    });
 
-   const municipality = await prisma.municipality.findUnique({
+    const municipality = await prisma.municipality.findUnique({
         where: {
             id: "0" // TODO: pass optional input
         }
-   });
-   const location = municipality?.locations ? randomFromArray(municipality?.locations, "farm") : "farm";
-   const name = `help ${giverName} solve problem at ${location} in ${municipality?.name}`;
+    });
+    const location = municipality?.locations ? randomFromArray(municipality?.locations, "farm") : "farm";
+    const name = `help ${giverName} solve problem at ${location} in ${municipality?.name}`;
 
     return {
         name: name,
