40f8f8758aa8ded2188d4b29c3fe697e7373b137
Tapollo <tylersimonich@gmail.com>
2023-02-19 19:22:40 -0700

    Attempt to fix Facilities skeleton

--- src/pages/guild/facilities/[id].tsx
+++ src/pages/guild/facilities/[id].tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import HeroPreview from "../../../components/Heroes/HeroPreview";
-import { trpc } from "../../../utils/trpc";
 
 const HeroesPage: NextPage = () => {
     const { asPath } = useRouter();
     const id = asPath.split('/').pop() ?? "";
-    const heroes = trpc.hero.getHeroesByGuild.useQuery({ id: id })?.data;
 
     return (
         <div>
             <h1 className="text-2xl my-8">Guild Facilities</h1>
-            <div className="cards">
-            </div>
             <div className="mt-8">
                 <Link href={`/guild/${id}`} className="btn">Back to guild</Link>
             </div>
