6a78b07c02f9a400838e0c40953e0d45b16eb736
Tapollo <tylersimonich@gmail.com>
2023-02-19 21:23:33 -0700

    added facility table to facility skeleton page

--- src/pages/guild/facilities/[id].tsx
+++ src/pages/guild/facilities/[id].tsx
@@ -10,9 +10,32 @@ const FacilitiesPage: NextPage = () => {
 
     return (
         <div>
-            <section>
-            <h1 className="text-2xl my-8">Guild Facilities</h1>
-            </section>
+            {guild && (
+                <section>
+                   <h1 className="text-2xl my-8">Guild Facilities</h1>
+                       <table className="text-center block md:table mt-8 md:mt-0">
+                            <caption className="text-lg block md:table-caption">Guild Facilities Levels</caption>
+                            <thead className="block md:table-header-group">
+                                <tr className="block md:table-row absolute -top-full md:top-auto -left-full md:left-auto md:relative">
+                                    <th className="tbl-cell">Mess Hall</th>
+                                    <th className="tbl-cell">Workshop</th>
+                                    <th className="tbl-cell">Training Ground</th>
+                                    <th className="tbl-cell">Barracks</th>
+                                    <th className="tbl-cell">Infirmary</th>
+                                </tr>
+                            </thead>
+                            <tbody className="block md:table-row-group">
+                                <tr className="block md:table-row">
+                                    <td className="flex justify-between md:table-cell tbl-cell"><span className="md:hidden">Mess Hall</span>{guild.messHallLevel}</td>
+                                    <td className="flex justify-between md:table-cell tbl-cell"><span className="md:hidden">Workshop</span>{guild.workshopLevel}</td>
+                                    <td className="flex justify-between md:table-cell tbl-cell"><span className="md:hidden">Training Ground</span>{guild.trainingGroundLevel}</td>
+                                    <td className="flex justify-between md:table-cell tbl-cell"><span className="md:hidden">Barracks</span>{guild.barracksLevel}</td>
+                                    <td className="flex justify-between md:table-cell tbl-cell"><span className="md:hidden">Infirmary</span>{guild.infirmaryLevel}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                </section>
+             )}
             <section>
             {`/guild/${id}` && (
                 <div className="mt-8">
