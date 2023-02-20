364e63f7e448b24da79961b0b3e25968882bd8a7
Tapollo <tylersimonich@gmail.com>
2023-02-19 19:41:08 -0700

    Another attempt to fix Facilities Skeleton

--- src/pages/guild/facilities/[id].tsx
+++ src/pages/guild/facilities/[id].tsx
@@ -2,18 +2,24 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const HeroesPage: NextPage = () => {
+const FacilitiesPage: NextPage = () => {
     const { asPath } = useRouter();
     const id = asPath.split('/').pop() ?? "";
 
     return (
         <div>
+            <section>
             <h1 className="text-2xl my-8">Guild Facilities</h1>
-            <div className="mt-8">
-                <Link href={`/guild/${id}`} className="btn">Back to guild</Link>
-            </div>
+            </section>
+            <section>
+            {`/guild/${id}` && (
+                <div className="mt-8">
+                    <Link href={`/guild/${id}`} className="btn">Back to guild</Link>
+                </div>
+            )}
+            </section>
         </div>
     )
 }
 
-export default HeroesPage;
+export default FacilitiesPage;
