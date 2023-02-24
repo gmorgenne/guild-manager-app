1bab9f0eab6886edfffa55cd509b78548d1c52d4
Tapollo <tylersimonich@gmail.com>
2023-02-24 09:16:32 -0700

    staff card fix

--- src/components/Staff/StaffPreview.tsx
+++ src/components/Staff/StaffPreview.tsx
@@ -11,18 +11,18 @@ const Preview = (staff : Staff): JSX.Element => {
   
     return (
         <>
-            <div className="flex justify-between p-4 bg-white">
-                <div className="mr-4 bg-yellow-100">
-                    <div>Level: {staff.level}</div>
-                    <div>Race: {staff.race}</div>
-                    <div>Job Class: {staff.jobClass}</div>
-                    <div>Job Speciality: {staff.jobSpec}</div>
+            <div className="border border-blue-900 flex justify-between p-4 bg-indigo-100 rounded-t-3xl">
+                <div className="border border-blue-900 rounded-2xl mr-4 bg-gray-300 p-1">
+                    <div className="flex justify-between">Level: {staff.level}</div>
+                    <div className="flex justify-between">Race: {staff.race}</div>
+                    <div className="flex justify-between">Job Class: {staff.jobClass}</div>
+                    <div className="flex justify-between">Job Speciality: {staff.jobSpec}</div>
                 </div>
                 <Image src="https://via.placeholder.com/100?text=Staff" alt="staff preview thumbnail" height={120} width={100} />
             </div>
-            <div className="bg-gray-300">
-                <table className="text-center mx-auto w-full border border-red-900 border-separate">
-                    <caption className="text-lg my-2">{staff.name}</caption>
+            <div className="bg-blue-400 rounded-b-3xl">
+                <table className="text-center mx-auto w-full border border-blue-900 border-separate rounded-b-3xl">
+                    <caption className="border border-blue-900 font-bold text-lg">{staff.name}</caption>
                     <thead>
                         <tr>
                             <th className="table-cell">Job Speciality</th>
