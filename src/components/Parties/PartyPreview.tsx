9773cf0fd31774e15368a252bbf67e2f31ab807d
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2023-03-05 17:09:34 -0600

    rework party quest summary and preview

--- src/components/Parties/PartyPreview.tsx
+++ src/components/Parties/PartyPreview.tsx
@@ -1,6 +1,6 @@
 import type { Hero, Party } from "@prisma/client";
-import Link from "next/link";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { trpc } from "../../utils/trpc";
 import HeroPreview from "../Heroes/HeroPreview";
 import PartyQuestSummary from "./PartyQuestSummary";
@@ -9,43 +9,51 @@ export interface PartyPreviewProps {
     heroes: Hero[]
     party: Party
 }
+type RenamePartyForm = {
+    NewPartyName: string;
+}
 const PartyPreview = ({ heroes, party }: PartyPreviewProps): JSX.Element => {
     const [partyName, setPartyName] = useState(party.name);
-    const renamePartyMutation = trpc.party.renameParty.useMutation({
-        onSuccess: (data) => {
-            setPartyName(data?.data?.party?.name);
-        }
+    const renamePartyMutation = trpc.party.renameParty.useMutation({});
+    //const partyEditable = party.questId ? party.questId != "0" : false;
+    const [renamingParty, setRenamingParty] = useState(false);
+    const { handleSubmit, register } = useForm<RenamePartyForm>({
+        defaultValues: { NewPartyName: partyName }
     });
-    const activeQuest = party.questId ? party.questId !== "0" : false;
-    const assignQuestAvailable = party.questId ? party.questId != "0" : false;
-
-    const assignPartyToQuest = () => {
-        console.log('assign party to quest');
-        window.location.assign('/quests');
-    }
     
-    const renameParty = (name: string) => {
-        renamePartyMutation.mutate({ id: party.id, name: name });
+    const renameParty = ({ NewPartyName }: RenamePartyForm) => {
+        renamePartyMutation.mutate({ id: party.id, name: NewPartyName });
+        setPartyName(NewPartyName);
+        setRenamingParty(false);
     }
 
     // TODO:
-    // if active quest, link to quest status page
     // if no active quest, allow party to be editable (swap heroes, assign to new quest, disband party)
     return (
-        <>
+        <section>
             <div className="lg:flex justify-between items-center">
-                <h2 className="text-xl my-2">
-                    <label>Name: </label>
-                    <input name="partyName" value={partyName} type="text" onChange={(e) => renameParty(e.target.value)} className="h-10 px-2" />
-                </h2>
-                <div>
-                    {/* Party Actions, if applicable */}
-                    {activeQuest && <Link href="#">Quest Status</Link>}
-                    {(!activeQuest && assignQuestAvailable) && <button onClick={assignPartyToQuest} className="btn">Assign Party To Quest</button>}
+                <div className="text-xl my-2">
+                    {renamingParty ? (
+                        <form noValidate onSubmit={handleSubmit(renameParty)}>
+                            <label>Name: </label>
+                            <input type="text" className="h-10 px-2" {...register("NewPartyName")} />
+                            <button type="submit" className="btn">Submit</button>
+                        </form>
+                    ): (
+                        <div className="flex items-center">
+                            <h2 className="text-3xl px-2">{partyName}</h2>
+                            <button className="btn" onClick={() => setRenamingParty(true)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"> 
+                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                </svg>
+                            </button>
+                        </div>
+                    )}
+                    
                 </div>
                 <span>Compatibility: <span className="font-bold">{`${(party.compatibility * 100).toFixed(2)}%`}</span></span>
             </div>
-            {party.questId && <PartyQuestSummary party={party} />}
+            <PartyQuestSummary party={party} />
             <div className="cards mb-4">
                 {heroes?.map((hero, i) => {
                     return (
@@ -55,7 +63,7 @@ const PartyPreview = ({ heroes, party }: PartyPreviewProps): JSX.Element => {
                     )
                 })}
             </div>
-        </>
+        </section>
     )
 }
 
--- src/components/Parties/PartyQuestSummary.tsx
+++ src/components/Parties/PartyQuestSummary.tsx
@@ -1,5 +1,6 @@
 import type { Party } from "@prisma/client";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Modal from "react-modal";
 import { trpc } from "../../utils/trpc";
@@ -10,19 +11,40 @@ export interface PartyQuestSummaryProps {
 
 const PartyQuestSummary = ({ party }: PartyQuestSummaryProps): JSX.Element => {
     const quest = trpc.party.getPartyQuest.useQuery({ id: party.questId ?? "0" })?.data;
-    const [modalOpen, setModalOpen] = useState(false);
     const cancelTrainingAvailable = party.questId ? party.questId === "0" : false;
+    const [show, setShow] = useState(party.questId && party.questId.length > 0);
+    const [modalOpen, setModalOpen] = useState(false);
     const [questSuccess, setQuestSuccess] = useState(false);
     const [questSummary, setQuestSummary] = useState("");
+    const router = useRouter();
     const processQuestMutation = trpc.quest.processQuest.useMutation({
         onSuccess: (data) => {
             setQuestSuccess(data.status);
             setQuestSummary(data.data);
         }
     });
+    const resetQuestMutation = trpc.party.assignPartyToQuest.useMutation({
+        onSuccess: () => {
+            setShow(false);
+        }
+    });
+    const assignPartyMutation = trpc.party.assignPartyToQuest.useMutation()
+
+    const assignPartyToTraining = () => {
+        assignPartyMutation.mutate({
+            questId: "0",
+            partyId: party.id
+        });
+        router.push(`/guild/facilities/training-grounds/${party.guildId}`);
+    };
+
+    const assignPartyToQuest = () => {
+        router.push('/quests');
+    }
 
     const cancelTraining = () => {
-        console.log('stop training');
+        // TODO: We need to compute results of training
+        resetQuestMutation.mutate({ partyId: party.id });
     }
 
     const completeQuest = () => {
@@ -33,37 +55,52 @@ const PartyQuestSummary = ({ party }: PartyQuestSummaryProps): JSX.Element => {
     }
 
     const resetPartyQuest = () => {
-        console.log('reset party quest');
         setModalOpen(false);
+        resetQuestMutation.mutate({ partyId: party.id });
     }
 
     return (
-        <div className="border-t-2 border-b-2 border-black my-4 text-center">
-            <h3 className="text-lg">Current Quest</h3>
-            <Link href={`/quests/${quest?.id}`}>
-                <div className="lg:flex justify-between items-center my-4">
-                    <h4>Quest: {quest?.name}</h4>
-                    <div>Location: {quest?.location}</div>
-                    <div>League Quest?: {quest?.leagueQuest ? "true" : "false"}</div>
+        <>
+            {show && (
+                <div className={`border-t-2 border-b-2 border-black my-4 text-center ${show ? "" : "hidden"}`}>
+                <h3 className="text-lg">Current Quest</h3>
+                {party.questId=== "0"
+                ? 
+                    <div><h4>Quest: {quest?.name}</h4></div>
+                :
+                    <Link href={`/quests/${quest?.id}`}>
+                        <div className="lg:grid grid-cols-3 my-4">
+                            <h4>Quest: {quest?.name}</h4>
+                            <div>Location: {quest?.location}</div>
+                            <div>League Quest?: {quest?.leagueQuest ? "true" : "false"}</div>
+                        </div>
+                    </Link>
+                }
+                {/* TODO: if quest completed show "See Quest Result button" OR always says "See Quest Status" and if quest not completed, content shows how long the quest has taken so far */}
+                {!cancelTrainingAvailable && <button className="btn" onClick={completeQuest}>See Quest Result</button>}
+                {cancelTrainingAvailable && <button onClick={cancelTraining} className="btn">Cancel Training</button>}
+                <Modal isOpen={modalOpen} onRequestClose={resetPartyQuest}>
+                    <button onClick={resetPartyQuest} className="btn--close">
+                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                    <h4 className="text-xl">Quest Summary Details</h4>
+                    <div className="my-8">
+                        <h5 className="text-lg mb-4">{questSuccess ? "Success!" : "FAIL!!"}</h5>
+                        <p>Here is what happened:</p>
+                        <div className="quest-summary" dangerouslySetInnerHTML={{__html: questSummary}}></div>
+                    </div>
+                </Modal>
+            </div>
+            )}
+            {!show && (
+                <div>
+                    <button onClick={assignPartyToQuest} className="btn">Assign Party To Quest</button>
+                    <button onClick={assignPartyToTraining} className="btn">Assign Party To Training</button>
                 </div>
-            </Link>
-            {/* TODO: if quest completed show "See Quest Result button" */}
-            {!cancelTrainingAvailable && <button className="btn" onClick={completeQuest}>See Quest Result</button>}
-            {cancelTrainingAvailable && <button onClick={cancelTraining} className="btn">Cancel Training</button>}
-            <Modal isOpen={modalOpen} onRequestClose={resetPartyQuest}>
-                <button onClick={resetPartyQuest} className="btn--close">
-                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-                <h4 className="text-xl">Quest Summary Details</h4>
-                <div className="my-8">
-                    <h5 className="text-lg mb-4">{questSuccess ? "Success!" : "FAIL!!"}</h5>
-                    <p>Here is what happened:</p>
-                    <div className="quest-summary" dangerouslySetInnerHTML={{__html: questSummary}}></div>
-                </div>
-            </Modal>
-        </div>
+            )}
+        </>
     )
-}
+};
 export default PartyQuestSummary;
