2303e45c593b25838c5426331ac254df3bb435f5
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2023-03-09 17:51:54 -0600

    move crew router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,4 +1,5 @@
 import { authRouter } from "./auth";
+import { crewRouter } from "./crew";
 import { encounterRouter } from './encounter';
 import { exampleRouter } from "./example";
 import { guildRouter } from './guild';
@@ -7,19 +8,17 @@ import { partyRouter } from './party';
 import { questRouter } from "./quest";
 import { router } from "../trpc";
 import { staffRouter } from "./staff";
-import { crewRouter } from "./crew";
-
 
 export const appRouter = router({
   auth: authRouter,
+  crew: crewRouter,
   encounter: encounterRouter,
   example: exampleRouter,
   guild: guildRouter,
   hero: heroRouter,
   party: partyRouter,
   quest: questRouter,
-  staff: staffRouter,
-  crew: crewRouter
+  staff: staffRouter
 });
 
 // export type definition of API
