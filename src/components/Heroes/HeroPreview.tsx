a874dc100da58706f2894e26f8a14dc5fda9af91
gmorgenne <46210422+gmorgenne@users.noreply.github.com>
2023-03-09 18:45:02 -0600

    reswizzle the hero preview

--- src/components/Heroes/HeroPreview.tsx
+++ src/components/Heroes/HeroPreview.tsx
@@ -9,7 +9,7 @@ export type HeroPreviewProps = {
 
 const Preview = (hero : Hero): JSX.Element => {
     const tableCellClass = (stat: number) => {
-        let className = "table-cell";
+        let className = "table-cell tbl-cell";
         if (stat > 8) {
             className += " font-bold";
         }
@@ -21,15 +21,17 @@ const Preview = (hero : Hero): JSX.Element => {
     
     return (
         <div className="border border-indigo-900 rounded-3xl">
-            <div className="flex justify-between p-4 bg-blue-100 rounded-t-3xl">
-                <div className="border border-indigo-900 rounded-2xl mr-4 bg-gray-300 p-1">
+            <div className="p-4 bg-blue-100 rounded-t-3xl">
+                <div className="border border-indigo-900 rounded-2xl mb-4 bg-gray-300 p-1">
                     <div className="flex justify-between gap-2">Level: <span>{hero.level}</span></div>
                     <div className="flex justify-between gap-2">Race: <span>{hero.race}</span></div>
                     <div className="flex justify-between gap-2">Class: <span>{hero.class}</span></div>
                     <div className="flex justify-between gap-2">Subclass: <span>{hero.subclass}</span></div>
                     <div className="flex justify-between gap-2">Alignment: <span className="ml-1">{hero.alignment}</span></div>
                 </div>
-                <div className="border border-indigo-900 flex"> <Image src="https://via.placeholder.com/100?text=Hero" alt="hero preview thumbnail" height={120} width={100} /> </div> 
+                <div>
+                    <Image src="https://via.placeholder.com/100?text=Hero" alt="hero preview thumbnail" className="m-auto" height={120} width={100} />
+                </div> 
             </div>
             <div className="bg-red-500">
                 <div className="bg-green-500 text-right" style={{ width: `${ (hero.healthPoints / hero.maxHealthPoints) * 100 }%`}}>&nbsp;</div>
@@ -40,22 +42,22 @@ const Preview = (hero : Hero): JSX.Element => {
                     <caption className="border border-indigo-900 font-bold text-lg">{hero.name}</caption>
                     <thead>
                         <tr>
-                            <th className="table-cell">Str</th>
-                            <th className="table-cell">Dex</th>
-                            <th className="table-cell">Mag</th>
-                            <th className="table-cell">Con</th>
-                            <th className="table-cell">Def</th>
-                            <th className="table-cell">Res</th>
+                            <th className="table-cell tbl-cell">Str</th>
+                            <th className="table-cell tbl-cell">Dex</th>
+                            <th className="table-cell tbl-cell">Mag</th>
+                            <th className="table-cell tbl-cell">Con</th>
+                            <th className="table-cell tbl-cell">Def</th>
+                            <th className="table-cell tbl-cell">Res</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td className={tableCellClass(hero.strength)}>{hero.strength}</td>
+                            <td className={tableCellClass(hero.strength) + " rounded-bl-3xl"}>{hero.strength}</td>
                             <td className={tableCellClass(hero.dexterity)}>{hero.dexterity}</td>
                             <td className={tableCellClass(hero.magic)}>{hero.magic}</td>
                             <td className={tableCellClass(hero.constitution)}>{hero.constitution}</td>
                             <td className={tableCellClass(hero.defense)}>{hero.defense}</td>
-                            <td className={tableCellClass(hero.resistance)}>{hero.resistance}</td>
+                            <td className={tableCellClass(hero.resistance) + " rounded-br-3xl"}>{hero.resistance}</td>
                         </tr>
                     </tbody>
                 </table>
